5e7d7dc9968aa39fcb73a1a5f9628cd786790d82
shotanue <shotanue@gmail.com>
2024-08-20 22:19:31 +0900

    chore: setup next

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { App } from "@repo/ui";
+import "@repo/ui/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <App txt="" />
+      </body>
     </html>
   );
 }
